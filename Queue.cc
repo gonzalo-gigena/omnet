ef7c4d493501d714a1b4ae8a743007e0f54f85cd
usuario <gongigena@hotmail.com>
2019-05-12 18:28:04 -0300

    Hopefully thjis is the one that solves the problem

--- Queue.cc
+++ Queue.cc
@@ -9,7 +9,7 @@ using namespace omnetpp;
 class Queue: public cSimpleModule {
 private:
     cQueue buffer;
-    cMessage *endServiceEvent;
+    cPacket *endServiceEvent;
     simtime_t serviceTime;
     cOutVector bufferSizeVector;
     cOutVector packetDropVector;
@@ -38,7 +38,7 @@ void Queue::initialize() {
     packetDropVector.setName("Packets Lost");
 
     buffer.setName("buffer");
-    endServiceEvent = new cMessage("endService");
+    endServiceEvent = new cPacket("endService");
 }
 
 void Queue::finish() {
